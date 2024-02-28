28974abe7d0343e3e0593f4dad9beeb7b6ef620c
YHYA-FR <ooppyhya1@gmail.com>
2024-02-28 08:28:59 -0800

    creat title home page

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
-class home extends StatelessWidget
-{
+class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return Material(
-   child:null,
-   );
+    return Material(
+      child: Scaffold(
+        body: Row(
+          children: [
+            const Text(
+              'NOTES',
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-  
 }
